e7b52f7ea7e469302cd3f6f94ead832d307a0259
Tushar Shandilya <tusharshandilya96@gmail.com>
2019-07-11 12:39:58 +0530

    added react-router-dom to menu-items and directory, updated menu items props in directory.js to be passed using es6 syntax

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import Homepage from './Pages/Homepage/Homepage';
+
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE</h1>
+  </div>
+)
 
 class App extends React.Component {
 
   render(){
     return (
       <div >
-        <Homepage />
+      <Switch >
+        <Route exact path='/' component={Homepage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
       </div>
     );
   }
