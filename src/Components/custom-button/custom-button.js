6dc35cfbde4c063bbb3629994531de66d77318cb
Tushar Shandilya <tusharshandilya96@gmail.com>
2019-09-13 20:15:45 +0530

    Added stripe payment component and changed site icon

--- src/Components/custom-button/custom-button.js
+++ src/Components/custom-button/custom-button.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
-  <button className={`${inverted ? 'inverted ': ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, inverted, paytm, ...otherProps}) => (
+  <button className={`${inverted ? 'inverted ': ''} ${paytm ? 'paytm' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
     {children}
   </button>
 );
